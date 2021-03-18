e1c17d194dee4487bd00e8fbd3809cf79bff3980
ksh24865 <8819889@naver.com>
2021-03-18 22:12:47 +0900

    modify DataGenerator

--- DataGenerator/DataGenerator.py
+++ DataGenerator/DataGenerator.py
@@ -7,7 +7,7 @@ import func
 # sink : 1(광나루), 2(잠실), 3(뚝섬), 4(잠원), 5(반포), 6(이촌), 7(여의도), 8(양화), 9(망원), 10(난지), 11(강서)
 # node : 각 공원당 12개씩
 
-# sensor : [1(udust), 2(dust), 3(humid)], [4(lux), 5(shumid)], [6(visitor)], [7(trash)] 
+# sensor : [1(udust), 2(dust), 3(humid),4(lux)(조도,오류체크),5(visitor)], [6(shumid)], , [7(trash)],  
 
 # 한가앙 이름 순으로 한바퀴씩 돌면서 node 4개씩 있다고 가정 
 
@@ -23,17 +23,14 @@ for node in range(3):
     for sink in range(11):
         nowline = line[i].rstrip().split(',')
         i+=1
-        for sensor in range(1,4):
-            data = {"sensor_id":sensor,"node_id": 12*sink+3*node+1,"values":[nowline[sensor]],"timestamp":timestamp}
-            func.produce_data(producer,data,conf.topic)
-        for sensor in range(4,6):
-            data = {"sensor_id":sensor,"node_id": 12*sink+3*node+2,"values":[nowline[sensor]],"timestamp":timestamp}
-            func.produce_data(producer,data,conf.topic)
+        for sensor in range(1,6):
+            data = {"sensor_id":sensor,"node_id": 9*sink+3*node+1,"values":[nowline[sensor]],"timestamp":timestamp}
+            func.produce_data(producer,data,conf.topic)        
         sensor=6
-        data = {"sensor_id":sensor,"node_id": 12*sink+3*node+3,"values":[nowline[sensor]],"timestamp":timestamp}
+        data = {"sensor_id":sensor,"node_id": 9*sink+3*node+2,"values":[nowline[sensor]],"timestamp":timestamp}
         func.produce_data(producer,data,conf.topic)
         sensor=7
-        data = {"sensor_id":sensor,"node_id": 12*sink+3*node+4,"values":[nowline[sensor]],"timestamp":timestamp}
+        data = {"sensor_id":sensor,"node_id": 9*sink+3*node+3,"values":[nowline[sensor]],"timestamp":timestamp}
         func.produce_data(producer,data,conf.topic)
     # time.sleep(30)
 
--- DataGenerator/DataMaker.py
+++ DataGenerator/DataMaker.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy
-
+# udust,dust,humid,lux,shumid,visitor,trash
+# sensor : [1(udust), 2(dust), 3(temp),3(humid),4(lux)(조도,오류체크),5(visitor)], [6(shumid)], , [7(trash)],  
 #기상정보
 air_df=pd.read_csv('DATA/스마트서울 도시데이터 센서(S-DoT) 환경정보/S-DoT07.csv') # 읽어오기
 air_df.drop(air_df[air_df['조도(lux)']  == 0].index,inplace=True) # 조도 0,null제거
 air_df.drop(air_df[numpy.isnan(air_df['조도(lux)'])].index,inplace=True)
-air_df = air_df[['초미세먼지(㎍/㎥)','미세먼지(㎍/㎥)','상대습도( %)','조도(lux)']] # 필요한 것만 인덱싱
+air_df = air_df[['초미세먼지(㎍/㎥)','미세먼지(㎍/㎥)','기온(℃)','상대습도( %)','조도(lux)']] # 필요한 것만 인덱싱
 
 #쓰레기통 정보
 trash_df = pd.read_csv('DATA/trashinfo.txt')
@@ -20,6 +21,6 @@ visitor_df = pd.read_csv('DATA/서울시 한강공원 이용객 현황 통계/re
 visitor_df = (visitor_df[['합계']]//27)[:597543] #필요한 것 597543개만 인덱싱
 
 #df 합친 후 저장
-result = pd.concat([air_df,soil_humid,visitor_df,trash_df],axis=1, join='inner') # 데이터 모두 합치기
-result.columns=["udust","dust","humid","lux","shumid","visitor","trash"] # col 이름 변경
+result = pd.concat([air_df,visitor_df,soil_humid,trash_df],axis=1, join='inner') # 데이터 모두 합치기
+result.columns=["udust","dust","temper","humid","lux","visitor","shumid","trash"] # col 이름 변경
 result.to_csv("DATA/data.csv",mode='w') # csv파일로 저장
