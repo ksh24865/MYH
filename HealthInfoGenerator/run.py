5db2b4cbc22f653eaa558768c9fa5d6ffe31d03b
ksh24865 <8819889@naver.com>
2021-03-18 22:10:24 +0900

    add HealthInfoGenerator

---
+++ HealthInfoGenerator/run.py
@@ -0,0 +1,28 @@
+import random
+import socket
+import time
+
+def get_state():
+    if random.randint(0,10) > 2:
+        return "true"
+    else:
+        return "false"
+def make_msg(sid):
+    msg=""
+    msg+='{'    
+    msg+='"sid": '+str(sid)+', "state": ['
+    for nid in range(1,10):
+        msg+='{"nid": '+str((sid-1)*9+nid)+', "state": '+get_state()+', "battery": '+str(100-nid)+'}'
+        if nid < 9:
+            msg+=','
+        else:
+            msg+=']}'
+    return msg
+
+
+for i in range(1,12):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+    sock.connect(('10.5.110.11', 8085)) 
+    sock.send(make_msg(i).encode())    
+    time.sleep(0.2)
+
